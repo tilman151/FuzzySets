ec6bb5a29e2e0d9b79d09499263289d8851cd48c
Tilman Krokotsch <t.krokotsch@gmail.com>
2016-10-31 16:14:17 +0100

    Added docstrings

--- alphaCuts.py
+++ alphaCuts.py
@@ -1,8 +1,23 @@
+"""The alphaCuts module.
+
+Module for the first assignment of Fuzzy Systems
+
+"""
 import collections
 
 
 class FuzzySet(collections.OrderedDict):
-    """docstring for ."""
+    """This class implements a horizontal representation of a fuzzy set.
+
+    The constructor receives a list of membership degrees. It is checked,
+    whether the degrees are in [0,1]. If not an exception is raised. The
+    ordered list of degrees is passed to the constructor of the super class.
+
+    Args:
+        degrees (:obj:`list` of float): List of membership degrees for the
+        alpha cuts of the fuzzy set
+
+    """
     def __init__(self, degrees):
         if (all(item <= 1.0 for item in degrees)
                 and all(item >= 0 for item in degrees)):
@@ -13,6 +28,14 @@ class FuzzySet(collections.OrderedDict):
         else:
             raise ValueError("Membership degrees have to be in [0,1]")
 
+    """
+    Returns the membership degree of an arbirary real number concerning this
+    fuzzy set.
+
+    Args:
+        instance (float): Instance of interest
+
+    """
     def getMembershipDegree(self, instance):
         for (key, list) in self.iteritems():
             for (begin, end) in list:
