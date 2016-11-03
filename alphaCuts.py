8c5eb54ac6c031c06186746d33315ccc6f4063a7
Tilman Krokotsch <t.krokotsch@gmail.com>
2016-11-03 23:17:34 +0100

    Revised documentation; made code prettier

--- alphaCuts.py
+++ alphaCuts.py
@@ -9,34 +9,41 @@ import collections
 class FuzzySet(collections.OrderedDict):
     """This class implements a horizontal representation of a fuzzy set.
 
-    The constructor receives a list of membership degrees. It is checked,
-    whether the degrees are in [0,1]. If not an exception is raised. The
-    ordered list of degrees is passed to the constructor of the super class.
+    After calling the constructor add an alpha cut using the []-Operator. An
+    alpha cut is represented as a list of tuples. Each tuple is an interval
+    of the cut.
 
-    Args:
-        degrees (:obj:`list` of float): List of membership degrees for the
-        alpha cuts of the fuzzy set
+    Example:
+        >>> import alphaCuts
+        >>> s = alphaCuts.FuzzySet([0.4,0.3,1.0])
+        >>> s[1.0] = [(1, 2), (4,6)]
 
     """
     def __init__(self, degrees):
-        if (all(item <= 1.0 for item in degrees) and
-                all(item >= 0 for item in degrees)):
-            super(FuzzySet, self).__init__(zip(
-                sorted(degrees, reverse=True),
-                [[] for x in xrange(len(degrees))]
-                ))
+        """Initialzize the fuzzy set with a finite number of membership degrees.
+
+        It is checked whether the memberships are in the real interval.
+
+        Args:
+            degrees (:obj:`list` of :obj:`int`): List of membership degrees
+
+        """
+        if all(0 <= item <= 1.0 for item in degrees):
+            emptyLists = [[] for x in xrange(len(degrees))]
+            dictData = zip(sorted(degrees, reverse=True), emptyLists)
+            super(FuzzySet, self).__init__(dictData)
         else:
             raise ValueError("Membership degrees have to be in [0,1]")
 
-    """
-    Returns the membership degree of an arbirary real number concerning this
-    fuzzy set.
-
-    Args:
-        instance (float): Instance of interest
-
-    """
     def getMembershipDegree(self, instance):
+        """
+        Return the membership degree of an arbirary real number concerning
+        this fuzzy set.
+
+        Args:
+            instance (float): Instance of interest
+
+        """
         for (key, list) in self.iteritems():
             for (begin, end) in list:
                 if (instance >= begin and instance <= end):
